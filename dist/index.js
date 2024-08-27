1fb15dfff2021babfd0287fe8290796c8ee47391
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-08-27 22:55:45 +0700

    preparing API pt1

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+require("dotenv/config");
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8000;
+const { PORT, API_URL } = process.env;
+app.use(body_parser_1.default.json());
 app.get("/", (req, res) => {
     res.send("Welcome to Ecommerce-Api-Nodejs server");
 });
-app.listen(port, () => {
-    console.log("Server is running at port: " + port);
+app.listen(PORT, () => {
+    console.log("Server is running at port: " + PORT);
+});
+app.get("/api/v1/products", (request, response) => {
+    const product = {
+        id: 1,
+        name: "hair dresser",
+        image: "img_url"
+    };
+    response.send(product);
+});
+app.get("/test", (request, response) => {
+    response.send({ msg: "Hello hello" });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
-import express, { Express, Request, Response, Application } from "express";
-
+import express, { Request, Response, Application, request } from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import morgan from "morgan";
+import mongoose from "mongoose";
 const app: Application = express();
-const port = process.env.PORT || 8000;
 
+const { PORT, API_URL, CONNECTTION_STRING } = process.env;
+
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Ecommerce-Api-Nodejs server");
 });
 
-app.listen(port, () => {
-    console.log("Server is running at port: " + port);
+app.listen(PORT, () => {
+  console.log("Server is running at port: " + PORT);
+});
+
+if (!CONNECTTION_STRING) {
+  throw new Error("Database connection string is not defined");
+}
+
+mongoose.connect(CONNECTTION_STRING)
+  .then(() => console.log("Connected to Database"))
+  .catch((error: Error) => { console.log(`Error: ${error}`) })
+
+
+
+app.get("/api/v1/products", (request: express.Request, response: Response) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "img_url"
+  }
+  response.send(product);
+
+});
+
+app.get("/test", (request: Request, response: Response) => {
+  response.send({ msg: "Hello hello" });
 })
+
+
