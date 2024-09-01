5f8b0ebacfe54397838a02a916055ba634dc2e3e
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-09-01 23:49:59 +0700

    CRUD categories

--- src/api/routes/category.ts
+++ src/api/routes/category.ts
@@ -5,11 +5,13 @@ import * as validation from "../utils/validationSchema";
 const router = Router();
 
 router.get("/api/v1/category", categorytHandler.getListCategory);
-
+router.get("/api/v1/category/:id", categorytHandler.getCategoryById);
 router.post("/api/v1/add-category",
     checkSchema(validation.addCategory_Validation_Schema),
     categorytHandler.addCategory);
 
 router.delete("/api/v1/delete-category/:id", categorytHandler.deleteCategory);
-router.put("/api/v1/update-category", categorytHandler.updateCategory);
+router.put("/api/v1/update-category/:id", 
+    checkSchema(validation.addCategory_Validation_Schema),
+    categorytHandler.updateCategory);
 export default router;
