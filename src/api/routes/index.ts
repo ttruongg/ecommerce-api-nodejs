2ade456a83f688ac7bfb024b3081a1dbaf0d7cf7
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-11-27 15:39:37 +0700

    orders management

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -3,14 +3,15 @@ import categoryRouter from "./category";
 import productRouter from "./product";
 import userRouter from "./user";
 import authentication from "./authentication";
-
+import orderRouter from "./order";
 const api = process.env.API_URL;
 const router: Router = Router();
 
 
-router.use(`${api}/categories`,categoryRouter);
-router.use(`${api}/products`,productRouter);
-router.use(`${api}/users`,userRouter);
+router.use(`${api}/categories`, categoryRouter);
+router.use(`${api}/products`, productRouter);
+router.use(`${api}/users`, userRouter);
+router.use(`${api}/orders`, orderRouter);
 router.use(authentication);
 
 export default router;
